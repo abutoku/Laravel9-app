cefce752dd264c8630e02251de6bf67a7ed16fde
abutoku <ystkz2021@gmail.com>
2023-01-03 00:24:27 +0900

    chapter-3 done

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -49,6 +49,7 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerateToken();
 
-        return redirect('/');
+        //return redirect('/');
+        return redirect('/tweet'); //ログアウト後のページ遷移
     }
 }
