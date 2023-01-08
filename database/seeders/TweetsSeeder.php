2e9c1bf856e561bda055675cdb752cb2c3963acd
abutoku <ystkz2021@gmail.com>
2023-01-08 14:19:54 +0900

    chapter4-4 画像アップロード seederの作成

--- database/seeders/TweetsSeeder.php
+++ database/seeders/TweetsSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Image;
 use Illuminate\Database\Seeder;
 use App\Models\Tweet;
 // use Illuminate\Support\Facades\DB;
@@ -17,7 +17,14 @@ class TweetsSeeder extends Seeder
      */
     public function run()
     {
-        Tweet::factory()->count(10)->create();
+        Tweet::factory()->count(10)->create()->each(fn($tweet) =>
+            Image::factory()->count(4)->create()->each(fn($image) =>
+                $tweet->images()->attach($image->id)
+            )
+        );
+
+        // Tweet::factory()->count(10)->create();
+
         // DB::table('tweets')->insert([
         //     'content' => Str::random(100),
         //     'created_at' => now(),
